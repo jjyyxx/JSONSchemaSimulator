f79bd649fd123f65239f81c7ed7fff7409a76c31
jjyyxx <obfishstudio@gmail.com>
2018-07-26 08:04:38 +0800

    version check

---
+++ bin/version_check.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process')
+const { TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, TRAVIS_PULL_REQUEST_BRANCH } = process.env
+
+if (TRAVIS_PULL_REQUEST && TRAVIS_BRANCH === 'master') {
+  if (TRAVIS_PULL_REQUEST_BRANCH === 'develop') {
+    const previous = JSON.parse(execSync('git show master:package.json').toString('utf8')).version
+    const current = JSON.parse(execSync('git show develop:package.json').toString('utf8')).version
+    if (previous === current) {
+      console.log('The version number did not increase')
+      process.exit(1)
+    }
+  } else {
+    console.log('Only develop can be merged into master')
+    process.exit(1)
+  }
+}
